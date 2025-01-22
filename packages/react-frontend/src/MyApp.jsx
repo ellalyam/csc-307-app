0756ad4edfd83bdff8dd63d5555896fb26e44ad9
Ella Yam <ellalyam@gmail.com>
2025-01-22 10:37:22 -0800

    IE 3 Step 3 Complete

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -5,6 +5,17 @@ import Form from "./Form";
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  /*function updateList(person) {
+    setCharacters([...characters, person]);
+  } */
+
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
@@ -19,15 +30,24 @@ function MyApp() {
       });
   }, []);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
     });
-    setCharacters(updated);
+  
+    return promise;
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
